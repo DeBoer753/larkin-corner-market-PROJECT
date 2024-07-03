4811697e35b3273c1f63c1f6943c74e5f2a39da7
Myles DeBoer <mylesdeboer@Myless-MacBook-Pro-2.local>
2024-07-02 22:47:25 -0700

    update: 4th of july image and message

--- src/Cards/A-BacksideData.js
+++ src/Cards/A-BacksideData.js
@@ -2,5 +2,6 @@
 export const storeName = "Larkin Corner Market"
 export const description1 = "Hello, and welcome to Larkin Corner Market! My name is Myles and I am a full time beverage sales rep / web developer hobbyist who created the card you are seeing now."
 export const description2 = "My goal is to help grow not only our portfolio of wines, beers, and spirits, but more importantly to grow the neighborhood stores we know and love here in San Francisco through a digital perspective."
-export const message = '"Weathers heating up! Scan the whites and roses to find out whats chilling up in the fridge!"'
+export const message = '"One day to go until the 4th 🇺🇸! Enjoy your day!"'
+// export const message = '"Weathers heating up! Scan the whites and roses to find out whats chilling up in the fridge!"'
 export const rescanTheQRCodeFor = "Rescan the QR codes during holidays and public events for updated themes and messages!"
