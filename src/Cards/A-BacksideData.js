bd3c3e5b2156e200896df623e752fb24304e4093
deboer753 <myles.deboer@gmail.com>
2025-03-13 18:43:22 -0700

    updated message

--- src/Cards/A-BacksideData.js
+++ src/Cards/A-BacksideData.js
@@ -2,5 +2,5 @@
 export const storeName = "Larkin Corner Market"
 export const description1 = "Hello, and welcome to Larkin Corner Market! My name is Myles and I am a full time beverage sales rep / web developer hobbyist who created the card you are seeing now."
 export const description2 = "My goal is to help grow not only our portfolio of wines, beers, and spirits, but more importantly to grow the neighborhood stores we know and love here in San Francisco through a digital perspective."
-export const message = '"Happy New Year San Francisco!"'
+export const message = '"Wine and beer go quite well with rainy weather too you know! Scan some more QR codes and see what you find!"'
 export const rescanTheQRCodeFor = "Rescan the QR codes during holidays and public events for updated themes and messages!"
